ea5081759179748cb7e85bf02b4e8d705fd913ff
Paulo Magno <pg.magno@hotmail.com>
2023-12-09 09:52:38 -0300

    creates DAO interfaces for classes Department and Salesman

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,6 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		
 		Department obj = new Department(1, "Books");
 		
 		Salesman s = new Salesman(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
---
+++ src/model/dao/SalesmanDAO.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Salesman;
+
+public interface SalesmanDAO {
+	void insert(Salesman obj);
+	void update(Salesman obj);
+	void deleteById(Integer id);
+	Salesman findById(Integer id);
+	List<Salesman> findAll();
+}
